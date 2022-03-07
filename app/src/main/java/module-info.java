9c4ab294a561909966a8d22bb76c755edb9f5693
Danman85 <d.versantvoort@hotmail.com>
2022-03-07 14:16:21 +0100

    Created and moved all relevant sources to the new 'Core' module

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module nl.danman.file_encryptor {
 
     requires org.apache.logging.log4j;
-    requires org.apache.commons.io;
     requires com.github.spotbugs.annotations;
 
     requires javafx.controls;
     requires javafx.fxml;
+
+    requires nl.danman.file_encryptor.service;
 
     opens nl.danman.file_encryptor.client.views.main to javafx.fxml;
     opens nl.danman.file_encryptor.client.views.file_encryptor to javafx.fxml;
